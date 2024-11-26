b927c9d32af734e24c093f867ec64340f8ac3e70
Lukhanyo Radebe <khanyoradebe46@gmail.com>
2024-11-27 01:03:48 +0200

    implement "create invoice" form

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -6,11 +6,23 @@ import {
     TableHead,
     TableHeader,
     TableRow,
-} from "@/components/ui/table"
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
-import { CirclePlus } from "lucide-react"
-import Link from "next/link";
+} from "@/components/ui/table";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import { CirclePlus } from "lucide-react";
+import {Label} from "@/components/ui/label";
+import {Input} from "@/components/ui/input";
+import {Textarea} from "@/components/ui/textarea";
 
 
 const Dashboard = () => {
@@ -21,13 +33,51 @@ const Dashboard = () => {
                     Invoices
                 </h1>
                 <p>
-                    <Button className="inline-flex gap-2" variant="ghost" asChild>
-                        <Link href="/invoices/new">
-                            <CirclePlus className="h-4 w-4"/>
-                            Create Invoice
-                        </Link>
+                    <AlertDialog>
+                        <AlertDialogTrigger asChild>
+                            <Button variant="outline">
+                                <CirclePlus className="h-4 w-4"/>
+                                Create Invoice
+                            </Button>
+                        </AlertDialogTrigger>
+                        <AlertDialogContent>
+                            <AlertDialogHeader>
+                                <AlertDialogTitle>
+                                    <div className="flex flex-col justify-center h-full gap max-w-5xl mx-auto my-4">
+                                        <div className="flex justify-between">
+                                            <h1 className="text-3xl font-bold mb-8">
+                                                Create a new invoice
+                                            </h1>
+                                        </div>
 
-                    </Button>
+                                        <form className="grid gap-4 max-w-xs">
+                                            <div>
+                                                <Label htmlFor="name" className="block font-semibold text-sm mb-2">Client Name</Label>
+                                                <Input id="name" name="name" type="text"/>
+                                            </div>
+                                            <div>
+                                                <Label htmlFor="email" className="block font-semibold text-sm mb-2">Client Email</Label>
+                                                <Input id="email" name="email" type="email"/>
+                                            </div>
+                                            <div>
+                                                <Label htmlFor="amount" className="block font-semibold text-sm mb-2">Amount</Label>
+                                                <Input id="amount" name="amount" type="text"/>
+                                            </div>
+                                            <div>
+                                                <Label htmlFor="description" className="block font-semibold text-sm mb-2">Description</Label>
+                                                <Textarea id="description" name="description"></Textarea>
+                                            </div>
+                                        </form>
+
+                                    </div>
+                                </AlertDialogTitle>
+                            </AlertDialogHeader>
+                            <AlertDialogFooter>
+                                <AlertDialogCancel>Cancel</AlertDialogCancel>
+                                <AlertDialogAction>Create</AlertDialogAction>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialog>
                 </p>
             </div>
 
@@ -48,7 +98,7 @@ const Dashboard = () => {
                             Status
                         </TableHead>
                         <TableHead className="text-right p-4">
-                            Value
+                            Amount
                         </TableHead>
                     </TableRow>
                 </TableHeader>
